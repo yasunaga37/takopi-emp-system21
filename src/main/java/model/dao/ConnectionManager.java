c6b561d9397e28b6d97ea124bddbfa30a54fa80d
yasunaga37 <yasunaga37@gmail.com>
2022-02-10 14:51:45 +0900

    ConnectionManagerをHeroku様に編集

--- src/main/java/model/dao/ConnectionManager.java
+++ src/main/java/model/dao/ConnectionManager.java
@@ -4,6 +4,8 @@
  */
 package model.dao;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -14,20 +16,20 @@ import java.sql.SQLException;
  */
 public class ConnectionManager {
 
-	/**
-	 * データベースURL
-	 */
-	private final static String URL = "jdbc:mysql://localhost:3306/em21_emp_sys_db?useSSL=false";
-
-	/**
-	 * ユーザ
-	 */
-	private final static String USER = "root";
-
-	/**
-	 * パスワード
-	 */
-	private final static String PASSWORD = "root";
+//	/**
+//	 * データベースURL
+//	 */
+//	private final static String URL = "jdbc:mysql://localhost:3306/em21_emp_sys_db?useSSL=false";
+//
+//	/**
+//	 * ユーザ
+//	 */
+//	private final static String USER = "root";
+//
+//	/**
+//	 * パスワード
+//	 */
+//	private final static String PASSWORD = "root";
 
 	/**
 	 * データベースへの接続を取得して返します。
@@ -35,11 +37,26 @@ public class ConnectionManager {
 	 * @return コネクション
 	 * @throws SQLException
 	 * @throws ClassNotFoundException
+	 * @throws URISyntaxException 
 	 */
 	public static Connection getConnection() throws SQLException, ClassNotFoundException {
+		
+        // heroku configに設定されている値を取得。
+    URI dbUri = null;
+	try {
+		dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
+	} catch (URISyntaxException e) {
+		e.printStackTrace();
+	}
+    // :をデリミタとして必要な情報を抜き取る。
+    String username = dbUri.getUserInfo().split(":")[0];
+    String password = dbUri.getUserInfo().split(":")[1];
+    // JDBC用のURLを生成。
+    String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
 
 		// JDBCドライバの読み込み
-		Class.forName("com.mysql.jdbc.Driver");
-		return DriverManager.getConnection(URL, USER, PASSWORD);
+//		Class.forName("com.mysql.jdbc.Driver");
+		Class.forName ("com.mysql.cj.jdbc.Driver");
+		return DriverManager.getConnection(dbUrl, username, password);
 	}
 }
